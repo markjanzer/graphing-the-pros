f33f0592b853e75e98d5976d7a1167ec819f8dc4
markjanzer <mark.janzer@gmail.com>
2018-09-28 00:19:36 -0700

    Update gemfile.

--- Gemfile
+++ Gemfile
@@ -7,8 +7,8 @@ gem 'wdm', '~> 0.1', platforms: [:mswin, :mingw]
 gem "middleman-sprockets", "~> 4"
 gem "sprockets-es6" 
 gem 'middleman-autoprefixer', '~> 2.7'
-gem "watir"
-gem "nokogiri"
+# gem "watir"
+# gem "nokogiri"
 gem "byebug"
 gem "httparty"
 
--- set_pro_data.rb
+++ set_pro_data.rb
@@ -1,9 +1,9 @@
-require "scraper.rb"
-require "sidekiq"
+# require "scraper.rb"
+# require "sidekiq"
 
-class Worker
-  include Sidekiq::Worker
-  def perform
-    config[:pro_data] = Scraper.new.response
-  end
-end
+# class Worker
+#   include Sidekiq::Worker
+#   def perform
+#     config[:pro_data] = Scraper.new.response
+#   end
+# end
